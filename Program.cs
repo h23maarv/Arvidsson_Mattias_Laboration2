9c8ee924895524dd9296a02bd00391fb43651e41
h23maarv <h23maarv@du.se>
2023-11-07 15:56:06 +0100

    Create Program.cs
    Skapade filen

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Arvidsson_Mattias_Labration1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
