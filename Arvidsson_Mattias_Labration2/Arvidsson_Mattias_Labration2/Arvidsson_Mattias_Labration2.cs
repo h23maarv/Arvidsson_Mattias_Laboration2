bae6f7a8cd5b2710e5b9a95ad75b3f1700159621
h23maarv <h23maarv@du.se>
2023-11-15 17:51:45 +0100

    La till så att användaren får välja antal personer

--- Arvidsson_Mattias_Labration2/Arvidsson_Mattias_Labration2/Arvidsson_Mattias_Labration2.cs
+++ Arvidsson_Mattias_Labration2/Arvidsson_Mattias_Labration2/Arvidsson_Mattias_Labration2.cs
@@ -4,13 +4,16 @@
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Hur många personer vill du lägga till?");
+            int numberofPeople = Convert.ToInt32(Console.ReadLine());
             // Skapar två arrayer för att lagra namnen och åldrarna på personerna.
-            string[] nameofRelativs = new string[5]; // Array för namn
-            int[] ageofRelativs = new int[5];        // Array för åldrar
-            int ageSum = 0;                          // Variabel för att hålla totala ålderssumman
+            string[] nameofRelativs = new string[numberofPeople]; // Array för namn
+            int[] ageofRelativs = new int[numberofPeople];        // Array för åldrar
+            int ageSum = 0;                                       // Variabel för att hålla totala ålderssumman
 
             // Loop för att mata in namn och åldrar från användaren för varje person.
-            for (int i = 1; i < nameofRelativs.Length; i++)
+            for (int i = 0; i < nameofRelativs.Length; i++)
             {
                 Console.WriteLine($"Vad heter person nummer {i}? ");
                 nameofRelativs[i] = Console.ReadLine();
@@ -21,7 +24,7 @@
             }
 
             // Loop för att skriva ut namnen och åldrarna för varje person samt räkna ålderssumman.
-            for (int i = 1; i < ageofRelativs.Length; i++)
+            for (int i = 0; i < ageofRelativs.Length; i++)
             {
                 Console.WriteLine("{0} är {1} år gammal.", nameofRelativs[i], ageofRelativs[i]);
                 ageSum += ageofRelativs[i];
