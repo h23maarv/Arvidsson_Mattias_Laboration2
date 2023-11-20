cb09cd96a5cde5f42d2bb0a08cac0a878c9f352d
h23maarv <h23maarv@du.se>
2023-11-20 11:27:02 +0100

    Fick error i Visual Studio så gjorde ett nytt projekt och klistrade in allt där

--- Arvidsson_Mattias_Labration1/Arvidsson_Mattias_Labration1/Program.cs
+++ Arvidsson_Mattias_Labration1/Arvidsson_Mattias_Labration1/Program.cs
@@ -1,19 +1,17 @@
-﻿namespace Arvidsson_Mattias_Labration2
+﻿namespace Arvidsson_Mattias_Labration1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Hur många personer vill du lägga till?");
-            int numberofPeople = Convert.ToInt32(Console.ReadLine());
-            // Skapar två arrayer för att lagra namnen och åldrarna på personerna.
-            string[] nameofRelativs = new string[numberofPeople]; // Array för namn
-            int[] ageofRelativs = new int[numberofPeople];        // Array för åldrar
-            int ageSum = 0;                                       // Variabel för att hålla totala ålderssumman
+            // Skapar tre arrayer för att lagra namnen och åldrarna på personerna.
+            string[] nameofRelativs = new string[5]; // Array för namn
+            int[] ageofRelativs = new int[5];        // Array för åldrar
+            int ageSum = 0;                          // Variabel för att hålla totala ålderssumman
 
             // Loop för att mata in namn och åldrar från användaren för varje person.
-            for (int i = 0; i < nameofRelativs.Length; i++)
+            for (int i = 1; i < nameofRelativs.Length; i++)
             {
                 Console.WriteLine($"Vad heter person nummer {i}? ");
                 nameofRelativs[i] = Console.ReadLine();
@@ -24,7 +22,7 @@
             }
 
             // Loop för att skriva ut namnen och åldrarna för varje person samt räkna ålderssumman.
-            for (int i = 0; i < ageofRelativs.Length; i++)
+            for (int i = 1; i < ageofRelativs.Length; i++)
             {
                 Console.WriteLine("{0} är {1} år gammal.", nameofRelativs[i], ageofRelativs[i]);
                 ageSum += ageofRelativs[i];
@@ -32,13 +30,11 @@
             }
 
             // Skriver ut den totala ålderssumman för alla släktingar.
-            Console.WriteLine("Gemensamt är alla " + ageSum + " gamla.");
+            Console.WriteLine("Gemensamt är alla " + ageSum + " år gamla.");
 
             // Beräknar genomsnittsåldern och skriver ut den med två decimaler.
             double averageAge = (double)ageSum / ageofRelativs.Length;
             Console.WriteLine("Genomsnitt ålder är: {0:0.00}.", averageAge);
-
-
 
         }
     }
---
+++ Arvidsson_Mattias_Labration2/Arvidsson_Mattias_Labration2/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace Arvidsson_Mattias_Labration2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+
+            Console.WriteLine("Hur många personer vill du lägga till?");
+            int numberofPeople = Convert.ToInt32(Console.ReadLine());
+
+            // Skapar två arrayer för att lagra namnen och åldrarna på personerna.
+            string[] nameofRelativs = new string[numberofPeople]; // Array för namn
+            int[] ageofRelativs = new int[numberofPeople];        // Array för åldrar
+            int ageSum = 0;                                       // Variabel för att hålla totala ålderssumman
+
+            while (numberofPeople > 0)
+            {
+                // Loop för att mata in namn och åldrar från användaren för varje person.
+                for (int i = 0; i < nameofRelativs.Length; i++)
+                {
+                    Console.WriteLine($"Vad heter person nummer {i}? ");
+                    nameofRelativs[i] = Console.ReadLine();
+
+                    Console.WriteLine($"Hur gammal är person nummer {i}? ");
+                    ageofRelativs[i] = Convert.ToInt32(Console.ReadLine());
+
+                }
+
+                // Loop för att skriva ut namnen och åldrarna för varje person samt räkna ålderssumman.
+                for (int i = 0; i < ageofRelativs.Length; i++)
+                {
+                    Console.WriteLine("{0} är {1} år gammal.", nameofRelativs[i], ageofRelativs[i]);
+                    ageSum += ageofRelativs[i];
+
+                }
+
+                // Skriver ut den totala ålderssumman för alla släktingar.
+                Console.WriteLine("Gemensamt är alla " + ageSum + " gamla.");
+
+                // Beräknar genomsnittsåldern och skriver ut den med två decimaler.
+                double averageAge = (double)ageSum / ageofRelativs.Length;
+                Console.WriteLine("Genomsnitt ålder är: {0:0.00}.", averageAge);
+
+                Console.WriteLine("Vill du fortsätta eller avsluta? ");
+                char answer = (char)Console.Read();
+                switch (answer)
+                { 
+                    
+                }
+
+                break;
+
+            }
+
+        }
+    }
+}
