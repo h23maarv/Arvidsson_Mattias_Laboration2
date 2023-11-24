d3650b45980ed9a35a51961f52094f12d59756e1
h23maarv <h23maarv@du.se>
2023-11-24 11:21:44 +0100

    Flyttat arrayerna samt lagt till if-sats i case 2-4 för att användaren ska få felmeddelande om case 1 inte har blivit kört.

--- Arvidsson_Mattias_Labration2/Arvidsson_Mattias_Labration2/Program.cs
+++ Arvidsson_Mattias_Labration2/Arvidsson_Mattias_Labration2/Program.cs
@@ -1,78 +1,121 @@
 ﻿namespace Arvidsson_Mattias_Labration2
 {
+
+    using System;
     internal class Program
     {
         static void Main(string[] args)
         {
 
-            bool running = true;
+            // Skapar arrayer för att lagra information.
+            int menuSelect = 0;
+            int numberofPeople = 0;
+            string[] nameofRelativs = null;                       // Array för namn
+            int[] ageofRelativs = null;                           // Array för åldrar
+            int ageSum = 0;                                       // Variabel för att se totala ålderssumman
 
-            while (running)
+            while (menuSelect != 5)
             {
 
-                Console.WriteLine("Hur många personer vill du lägga till?");
-                int numberofPeople = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(
+                    "\n------------------------------------------------" +
+                    "\nVälj ett av alternativen: " +
+                    "\n1.) Skriv in antal personer samt namn och ålder. " +
+                    "\n2.) Skriv ut namn samt ålder. " +
+                    "\n3.) Skriv ut den totala summan av de tillagda personers ålder. " +
+                    "\n4.) Skriv ut genomsnitt åldern för de tillagda personerna. " +
+                    "\n5.) Avsluta programmet. " +
+                    "\n------------------------------------------------"
+                    );
 
-                // Skapar tre arrayer för att lagra namnen och åldrarna på personerna.
-                string[] nameofRelativs = new string[numberofPeople]; // Array för namn
-                int[] ageofRelativs = new int[numberofPeople];        // Array för åldrar
-                int ageSum = 0;                                       // Variabel för att hålla totala ålderssumman
+                menuSelect = Int32.Parse(Console.ReadLine());
 
-                // Loop för att mata in namn och åldrar från användaren för varje person.
-                for (int i = 0; i < nameofRelativs.Length; i++)
-                {
-                    Console.WriteLine($"Vad heter person nummer {i}? ");
-                    nameofRelativs[i] = Console.ReadLine();
-
-                    Console.WriteLine($"Hur gammal är person nummer {i}? ");
-                    ageofRelativs[i] = Convert.ToInt32(Console.ReadLine());
-
-                }
-
-                // Loop för att skriva ut namnen och åldrarna för varje person samt räkna ålderssumman.
-                for (int i = 0; i < ageofRelativs.Length; i++)
-                {
-                    Console.WriteLine("{0} är {1} år gammal.", nameofRelativs[i], ageofRelativs[i]);
-                    ageSum += ageofRelativs[i];
-
-                }
-
-                // Skriver ut den totala ålderssumman för alla personer.
-                Console.WriteLine("Gemensamt är alla " + ageSum + " år gamla.");
-
-                // Beräknar genomsnittsåldern och skriver ut den med två decimaler.
-                double averageAge = (double)ageSum / ageofRelativs.Length;
-                Console.WriteLine("Genomsnitt ålder är: {0:0.00}.", averageAge);
-                
-                string answer;
-                Console.WriteLine("Vill du fortsätta eller avsluta? ja eller nej? ");
-
-                do
+                switch (menuSelect)
                 {
 
-                        answer = (Console.ReadLine());
+                    case 1:
 
-                        switch (answer)
+                        Console.WriteLine("Hur många personer vill du lägga till?");
+                        numberofPeople = Convert.ToInt32(Console.ReadLine());
+
+                        nameofRelativs = new string[numberofPeople];
+                        ageofRelativs = new int[numberofPeople];
+
+                        // Loop för att mata in namn och åldrar från användaren för varje person.
+                        for (int i = 0; i < numberofPeople; i++)
                         {
+                            Console.WriteLine($"Vad heter person nummer {i + 1}? ");
+                            nameofRelativs[i] = Console.ReadLine();
 
-                            case "ja":
-                                Console.WriteLine("Då börjar vi om ");
-                                break;
-
-                            case "nej":
-                                Console.WriteLine("Då avslutar vi programmet ");
-                                running = false;
-                                break;
-
-                            default:
-                                Console.WriteLine("Det är ett svar jag inte förstår... ");
-                                Console.WriteLine("Vill du fortsätta eller avsluta? ja eller nej? ");
-                                break;
-
+                            Console.WriteLine($"Hur gammal är person nummer {i + 1}? ");
+                            ageofRelativs[i] = Convert.ToInt32(Console.ReadLine());
                         }
 
-                    }
-                    while (answer != "ja" && answer != "nej");
+                        break;
+
+                    case 2:
+
+                        if (nameofRelativs != null && ageofRelativs != null)
+                        {
+                            // Loop för att skriva ut namnen och åldrarna för varje person samt räkna ålderssumman.
+                            for (int i = 0; i < numberofPeople; i++)
+                            {
+                                Console.WriteLine("{0} är {1} år gammal.", nameofRelativs[i], ageofRelativs[i]);
+                                ageSum += ageofRelativs[i];
+                            }
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("Inga personer har lagts till ännu. ");
+                        }
+
+                        break;
+
+                    case 3:
+
+                        if (nameofRelativs != null && ageofRelativs != null)
+                        {
+                            // Loop för att skriva ut den totala ålderssumman för alla personer.
+                            Console.WriteLine("Gemensamt är alla " + ageSum + " år gamla.");
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("Inga personer har lagts till ännu. ");
+                        }
+
+                        break;
+
+                    case 4:
+
+                        if (nameofRelativs != null && ageofRelativs != null)
+                        {
+                            // Beräknar genomsnittsåldern och skriver ut den med två decimaler.
+                            double averageAge = (double)ageSum / ageofRelativs.Length;
+                            Console.WriteLine("Genomsnitt ålder är: {0:0.00}.", averageAge);
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("Inga personer har lagts till ännu. ");
+                        }
+
+                        break;
+
+                    case 5:
+
+                        Console.WriteLine("Då avslutar vi programmet. ");
+
+                        break;
+
+                    default:
+
+                        Console.WriteLine("Det är ett svar jag inte förstår... Du behöver ange en siffra mellan 1-5.\n");
+
+                        break;
+                }
+ 
             }
 
         }
--- Arvidsson_Mattias_Labration2/Arvidsson_Mattias_Labration2/obj/Debug/net6.0/Arvidsson_Mattias_Labration2.AssemblyInfo.cs
+++ Arvidsson_Mattias_Labration2/Arvidsson_Mattias_Labration2/obj/Debug/net6.0/Arvidsson_Mattias_Labration2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Arvidsson_Mattias_Labration2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1576626caa41e370420b9a8d6bb2f845af6e252")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6150917a0d661191bb38fa91e607e92c55be9b89")]
 [assembly: System.Reflection.AssemblyProductAttribute("Arvidsson_Mattias_Labration2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Arvidsson_Mattias_Labration2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
