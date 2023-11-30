73358d2b6930a1566e7db493348482b116d6c69e
h23maarv <h23maarv@du.se>
2023-11-30 11:20:37 +0100

    Ändrade tillbaka namnet då det inte gick att öppna

---
+++ Arvidsson_Mattias_Labration3/Arvidsson_Mattias_Labration3/Gender.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arvidsson_Mattias_Labration3
+{
+	public enum Gender
+	{
+		Woman,
+		Man,
+		Nonbinary,
+		Other
+	}
+}
